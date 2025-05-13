8608a71283f28fd23ba404e0163bba1ef4cb11a3
SaniJ28 <157455192+SaniJ28@users.noreply.github.com>
2025-05-13 12:46:02 +0530

    2025-04-21

---
+++ 2145. Count the Hidden Sequences.cpp
@@ -0,0 +1,16 @@
+// Link-https://leetcode.com/problems/count-the-hidden-sequences/description/?envType=daily-question&envId=2025-04-21
+class Solution {
+public:
+    int numberOfArrays(vector<int>& differences, int lower, int upper) {
+        int x = 0, y = 0, cur = 0;
+        for (int d : differences) {
+            cur += d;
+            x = min(x, cur);
+            y = max(y, cur);
+            if (y - x > upper - lower) {
+                return 0;
+            }
+        }
+        return (upper - lower) - (y - x) + 1;
+    }
+};
