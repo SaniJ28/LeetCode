d873084a3bb5767d7acfa400b2d7c3b9cb78609b
SaniJ28 <157455192+SaniJ28@users.noreply.github.com>
2025-05-14 10:31:05 +0530

    2025-05-06

---
+++ 1920. Build Array from Permutation.cpp
@@ -0,0 +1,13 @@
+//Link- https://leetcode.com/problems/build-array-from-permutation/description/?envType=daily-question&envId=2025-05-06
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        for (int i = 0; i < n; ++i) {
+            ans.push_back(nums[nums[i]]);
+        }
+        return ans;
+    }
+};
