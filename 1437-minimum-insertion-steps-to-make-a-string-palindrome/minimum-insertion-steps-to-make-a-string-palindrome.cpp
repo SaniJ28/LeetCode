1c64e22a1fe2732e05f3fa020ee5e6a743d29ea8
SaniJ28 <157455192+SaniJ28@users.noreply.github.com>
2026-06-06 18:51:53 +0530

    Time: 19 ms (89.05%) | Memory: 9.7 MB (94.81%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minInsertions(string text1) {
+        string text2=text1;
+        reverse(text2.begin(),text2.end());
+        int n=text1.length();
+        vector<int>cur(n+1,0),prev(n+1,0);
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=n;ind2++){
+                if(text1[ind1-1]==text2[ind2-1])cur[ind2]=1+prev[ind2-1];
+                else{
+                    cur[ind2]=max(prev[ind2],cur[ind2-1]);
+                }
+            }
+            prev=cur;
+        }
+        return n-prev[n];
+    }
+};
