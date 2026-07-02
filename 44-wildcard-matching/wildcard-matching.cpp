c91666f372b260ce874bb2e9e39a61c17f931ff4
SaniJ28 <157455192+SaniJ28@users.noreply.github.com>
2026-07-02 11:12:28 +0530

    Time: 40 ms (71.93%) | Memory: 54.4 MB (27.88%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int func(string& s,string& p, int ind1,int ind2, vector<vector<int>>& dp){
-        if(ind1<0 && ind2<0) return 1;
+        if(ind1<0 && ind2<0) return true;
         if(ind1<0){
             for(int i=0;i<=ind2;i++){
-                if(p[i]!='*') return 0;
+                if(p[i]!='*') return false;
             }
-            return 1;
-        };
-        if(ind2<0)return 0;
+            return true;
+        }
+        if(ind2<0)return false;
 
         if(dp[ind1][ind2]!=-1)return dp[ind1][ind2];
 
         if(s[ind1]==p[ind2]){
             return dp[ind1][ind2]=func(s,p,ind1-1,ind2-1,dp);
         }
-        else if(p[ind2]=='?')return dp[ind1][ind2]= func(s,p,ind1-1,ind2-1,dp);
-        else if(p[ind2]=='*') return dp[ind1][ind2]=func(s,p,ind1-1,ind2,dp)||func(s,p,ind1,ind2-1,dp);
-        else if(s[ind1]!=p[ind2]) return dp[ind1][ind2]=0;
-        return 0;
+        if(p[ind2]=='?')return dp[ind1][ind2]= func(s,p,ind1-1,ind2-1,dp);
+        if(p[ind2]=='*') return dp[ind1][ind2]=func(s,p,ind1-1,ind2,dp)||func(s,p,ind1,ind2-1,dp);
+        return dp[ind1][ind2]=false;
     }
 
     bool isMatch(string s, string p) {
