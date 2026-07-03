a829064e908e71995c98ca6f774373d5a4512c05
SaniJ28 <157455192+SaniJ28@users.noreply.github.com>
2026-07-03 13:10:28 +0530

    Time: 135 ms (59.44%) | Memory: 139.5 MB (56.66%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
 
-    // int funct(vector<int>& prices,int ind, int buy, vector<vector<vector<int>>>& dp,int n,int tNo){
-    //     if(ind==n || tNo==0)return 0;
-    //     if(dp[ind][buy][tNo]!=-1)return dp[ind][buy][tNo];
+    int funct(vector<int>& prices,int ind, int trans, vector<vector<int>>& dp,int n){
+        if(ind==n|| trans==4)return 0;
+        
+        if(dp[ind][trans]!=-1)return dp[ind][trans];
 
-    //     int profit=0;
-    //     if(buy){
-    //         profit=max(-prices[ind]+ funct(prices, ind+1, 0,dp,n,tNo),funct(prices,ind+1,1,dp,n,tNo));
-    //     }
-    //     else profit=max(prices[ind]+ funct(prices, ind+1, 1,dp,n,tNo-1),funct(prices,ind+1,0,dp,n,tNo));
-    //     return dp[ind][buy][tNo]=profit;
-    // }
+        int profit=0;
+        if(trans%2==0){
+            profit=max(-prices[ind]+ funct(prices, ind+1, trans+1,dp,n),funct(prices,ind+1,trans,dp,n));
+        }
+        else profit=max(prices[ind]+ funct(prices, ind+1,trans+1,dp,n),funct(prices,ind+1,trans,dp,n));
+        return dp[ind][trans]=profit;
+    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>after(2,vector<int>(3,0));
-        vector<vector<int>>cur(2,vector<int>(3,0));
-        // funct(prices,0,1,dp,n,2);
+        vector<vector<int>>dp(n+1,vector<int>(5,0));
+        // vector<vector<int>>after(2,vector<int>(3,0));
+        // vector<vector<int>>cur(2,vector<int>(3,0));
+        // return funct(prices,0,0,dp,n);
         for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-                for(int tNo=1;tNo<3;tNo++){
-                    if(buy){
-                        cur[buy][tNo]=max(-prices[ind]+ after[0][tNo],after[1][tNo]);
-                     }
-                     else{
-                        cur[buy][tNo]=max(prices[ind]+after[1][tNo-1],after[0][tNo]);
-                     }
+            for(int trans=0;trans<4;trans++){
+                if(trans%2==0){
+                    dp[ind][trans]=max(-prices[ind]+dp[ind+1][trans+1],dp[ind+1][trans]);
                 }
+                else{
+                    dp[ind][trans]=max(prices[ind]+dp[ind+1][trans+1],dp[ind+1][trans]);
+                }               
             }
-            after=cur;
         }
-        return after[1][2];
+        return dp[0][0];
     }
 };
