492f20b41e2fe36279357f6b0c54ceea0f2e6aef
SaniJ28 <157455192+SaniJ28@users.noreply.github.com>
2026-07-03 12:31:18 +0530

    Time: 51 ms (69.51%) | Memory: 79.4 MB (72.57%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -14,20 +14,22 @@ public:
     // }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>>after(2,vector<int>(3,0));
+        vector<vector<int>>cur(2,vector<int>(3,0));
         // funct(prices,0,1,dp,n,2);
         for(int ind=n-1;ind>=0;ind--){
             for(int buy=0;buy<=1;buy++){
                 for(int tNo=1;tNo<3;tNo++){
                     if(buy){
-                        dp[ind][buy][tNo]=max(-prices[ind]+ dp[ind+1][0][tNo],dp[ind+1][1][tNo]);
+                        cur[buy][tNo]=max(-prices[ind]+ after[0][tNo],after[1][tNo]);
                      }
                      else{
-                        dp[ind][buy][tNo]=max(prices[ind]+ dp[ind+1][1][tNo-1],dp[ind+1][0][tNo]);
+                        cur[buy][tNo]=max(prices[ind]+after[1][tNo-1],after[0][tNo]);
                      }
                 }
             }
+            after=cur;
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 };
