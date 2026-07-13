a787e937c6c2a4e56a0044fb00e5f286bdcce19b
SaniJ28 <157455192+SaniJ28@users.noreply.github.com>
2026-07-13 23:24:19 +0530

    Time: 3 ms (36.97%) | Memory: 19.7 MB (54.70%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n=isConnected.size();
+        vector<int> vis(n,0);
+        int count=0;
+        queue<int> q;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){ 
+                count++;
+                q.push(i);
+                vis[i]=1;
+                while(!q.empty()){
+                    int node=q.front();
+                    q.pop();
+                    for(int j=0;j<n;j++){
+                        if(isConnected[node][j]==1 && vis[j]!=1){
+                            vis[j]=1;
+                            q.push(j);
+                        }
+                    }
+                }
+            }
+       }
+        return count;
+    }
+};
