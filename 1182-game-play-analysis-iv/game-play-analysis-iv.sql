3db86dce55584fcdcbde4df7a90ca5946fe200d2
SaniJ28 <157455192+SaniJ28@users.noreply.github.com>
2026-07-19 14:21:25 +0530

    Time: 517 ms (97.05%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,17 +1,15 @@
-SELECT
-  ROUND(
-    COUNT(A1.player_id)
-    / (SELECT COUNT(DISTINCT A3.player_id) FROM Activity A3)
-  , 2) AS fraction
-FROM
-    ACTIVITY A1
-WHERE
-    (A1.player_id, DATE_SUB(A1.event_date, INTERVAL 1 DAY)) IN (
-    SELECT
-      A2.player_id,
-      MIN(A2.event_date)
-    FROM
-      Activity A2
-    GROUP BY
-      A2.player_id
-  );
+with firstLogins as(
+    select a.player_id, min(a.event_date) as firstlogin
+    from activity a
+    group by a.player_id
+),
+consecLogin as (
+    select count(a.player_id) as num_logins
+    from firstlogins f
+    inner join activity a on f.player_id=a.player_id 
+    and f.firstLogin=Date_sub(a.event_date,interval 1 day)
+)
+select
+    round(
+        (select c.num_logins from conseclogin c)/
+        (select count(f.player_id)from firstLogins f),2) as fraction
