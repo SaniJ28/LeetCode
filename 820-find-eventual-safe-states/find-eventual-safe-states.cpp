473f48a1e1f669e04c42f0fbdc9db6e106938f6e
SaniJ28 <157455192+SaniJ28@users.noreply.github.com>
2026-08-10 19:50:20 +0530

    Time: 52 ms (35.74%) | Memory: 66.3 MB (36.28%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,35 +1,59 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& graph,vector<int>& vis, vector<int>& path,
-             vector<int>& safe) {
-        vis[node] = 1;
-        path[node] = 1;
-        for (int it : graph[node]) {
-            if (vis[it] == 0) {
-                if (dfs(it, graph, vis, path, safe) == true) {
-                safe[node] = 0;
-                return true;
-                }
-            } else if (path[it]) {
-                safe[node] = 0;
-                return true;
-            }
-        }
-        safe[node] = 1;
-        path[node] = 0;
-        return false;
-    }
+    // bool dfs(int node, vector<vector<int>>& graph,vector<int>& vis, vector<int>& path,
+    //     vector<int>& safe) {
+    //     vis[node] = 1;
+    //     path[node] = 1;
+    //     for (int it : graph[node]) {
+    //         if (vis[it] == 0) {
+    //             if (dfs(it, graph, vis, path, safe) == true) {
+    //             safe[node] = 0;
+    //             return true;
+    //             }
+    //         } else if (path[it]) {
+    //             safe[node] = 0;
+    //             return true;
+    //         }
+    //     }
+    //     safe[node] = 1;
+    //     path[node] = 0;
+    //     return false;
+    // }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> vis(n, 0), path(n, 0), safe(n, 0), ans;
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                dfs(i, graph, vis, path, safe);
+        // vector<int> vis(n, 0), path(n, 0), safe(n, 0), ans;
+        // for (int i = 0; i < n; i++) {
+        //     if (!vis[i]) {
+        //         dfs(i, graph, vis, path, safe);
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(safe[i]==1) ans.push_back(i);
+        // }
+        // return ans;
+        vector<int> topo;
+        vector<int>indegree(n);
+        queue<int>q;
+        vector<vector<int>> rev(n);
+        for(int i=0;i<n;i++){
+            for(int it: graph[i]){
+                rev[it].push_back(i);
+                indegree[i]++;
             }
         }
         for(int i=0;i<n;i++){
-            if(safe[i]==1) ans.push_back(i);
+            if(indegree[i]==0)q.push(i);
         }
-        return ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(int it: rev[node]){
+                indegree[it]--;
+                if(indegree[it]==0)q.push(it);
+            }
+        }
+        sort(topo.begin(),topo.end());
+        return topo;
     }
 };
