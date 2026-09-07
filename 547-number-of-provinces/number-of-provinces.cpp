21d28e949d79b41b066db5373f4608219320fb6b
SaniJ28 <157455192+SaniJ28@users.noreply.github.com>
2026-09-07 17:16:02 +0530

    Time: 4 ms (24.42%) | Memory: 20.2 MB (40.47%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,27 +1,49 @@
+class DSU{
+    public:
+    vector<int>size,parent;
+    DSU(int n){
+        size.resize(n,1);
+        parent.resize(n);
+        for(int i=0;i<n;i++)parent[i]=i;
+    }
+    int findParent(int node){
+        if(node==parent[node])return node;
+        return parent[node]=findParent(parent[node]);
+    }
+    void unionBySize(int u,int v){
+        int pu=findParent(u);
+        int pv=findParent(v);
+        if(pu==pv)return;
+        if(size[pu]<size[pv]){
+            size[pv]+=size[pu];
+            parent[pu]=pv;
+        }
+        else{
+            size[pu]+=size[pv];
+            parent[pv]=pu;
+        }
+    }
+};
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
+        vector<pair<int,int>>edges;
         int n=isConnected.size();
-        vector<int> vis(n,0);
-        int count=0;
-        queue<int> q;
         for(int i=0;i<n;i++){
-            if(!vis[i]){ 
-                count++;
-                q.push(i);
-                vis[i]=1;
-                while(!q.empty()){
-                    int node=q.front();
-                    q.pop();
-                    for(int j=0;j<n;j++){
-                        if(isConnected[node][j]==1 && vis[j]!=1){
-                            vis[j]=1;
-                            q.push(j);
-                        }
-                    }
-                }
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1)edges.push_back({i,j});
             }
-       }
+        }
+        DSU dsu(n);
+        for(auto it: edges){
+            int u=it.first;
+            int v=it.second;
+            dsu.unionBySize(u,v);
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(dsu.parent[i]==i)count++;
+        }
         return count;
     }
 };
